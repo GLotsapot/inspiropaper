eea25de17dd205515a1e915fdee27a853a72e798
Sawyer Peacock <sawyer@necrosoft.ca>
2021-08-13 02:09:35 -0400

    Initial commit

---
+++ InspiroPaper.ps1
@@ -0,0 +1,44 @@
+﻿function Folder-Clean {
+    param (
+        [Parameter(Mandatory)]
+        $FolderPath,
+        [int]
+        $FilesToKeep = 25
+    )
+
+    # Check folder and remove excess files... randomly
+    while($true){
+        $files = Get-ChildItem -File -Path $FolderPath
+        if($files.Count -ge $FilesToKeep)
+        {
+            $randomFileNum = Get-Random -Minimum 0 -Maximum $($FilesToKeep - 1)
+            Write-Host "Deleting random inspiration #$randomFileNum"
+            $files[$randomFileNum].Delete()
+        } else {
+            Write-Host "Ready to be inspired"
+            break
+        }
+    }
+}
+
+function Get-Image {
+    param (
+        [Parameter(Mandatory)]
+        $FolderPath
+    )
+
+    #Download new image
+    $imageUri = Invoke-RestMethod -Uri https://inspirobot.me/api?generate=true -UserAgent "InspiroPaper"
+    $filepath = Join-Path -Path $FolderPath -ChildPath $(Split-Path $imageUri -Leaf)
+    Invoke-WebRequest -Uri $imageUri -OutFile $filepath -UserAgent "InspiroPaper"
+    Write-Host "I feel inspired"
+}
+
+# Make sure the data folder exists
+$path = Join-Path -Path $Env:ProgramData -ChildPath "inspiro"
+if(!(Test-Path $path)) { New-Item -Path $path -ItemType "directory" }
+Write-Host "Saving inspiration to $path"
+
+
+Folder-Clean -FolderPath $path -FilesToKeep 50
+Get-Image -FolderPath $path
---
+++ README.md
@@ -0,0 +1,24 @@
+# InspiroPaper
+A simple script used to download images from the InspiroBot WebAPI to a folder.
+Typically the folder is used by Windows 10+ lock screens, or wallpaper in Slideshow mode
+
+## Prerequisites
+What things you need to install the software and how to install them
+```
+Powershell
+```
+
+
+## Implementation
+
+### Running Manually
+Run the powershell script by itself to download a new image
+
+### Scheduled Task
+1) Open Windows Task Scheduler
+2) Create a new Basic Task that runs the script
+
+### Displaying the sweet spoils
+1) Set either the lock screen or wallpaper in slideshow mode
+2) Add in the inspirobot folder which is showed when you run the task manually
+3) Become enlightened
